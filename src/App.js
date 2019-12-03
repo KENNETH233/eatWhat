65e69eb397862e222eea775d9691988c7a3ebbeb
Kenneth666 <kenneth666@Kenneth666deMacBook-Pro.local>
2019-12-03 11:26:10 +0800

    random picking number function

--- src/App.js
+++ src/App.js
@@ -1,83 +1,90 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-// import './App.css';
+import { DEFAULT_ECDH_CURVE } from 'tls';
+import './App.css';
 
-// const DEFAULT = [
-//   {
-//     dish: "Thai Food",
-//     // images: "".
-//     description: "Curry and Tom Yam Gong",
-//     filtered: "false"
-//   },
+const DEFAULT = [
+  {
+    dish: "Thai Food",
+    // images: "".
+    description: "Curry and Tom Yam Gong",
+    filtered: "false"
+  },
 
-//   {
-//     dish: "Dim Sum",
-//     // images: "".
-//     description: "Shrimps dumplings, Chicken feets",
-//     filtered: "false"
-//   },
+  {
+    dish: "Dim Sum",
+    // images: "".
+    description: "Shrimps dumplings, Chicken feets",
+    filtered: "false"
+  },
 
-//   {
-//     dish: "Hotpots",
-//     // images: "".
-//     description: "Sichuan style, Spicy",
-//     filtered: "false"
-//   },
+  {
+    dish: "Hotpots",
+    // images: "".
+    description: "Sichuan style, Spicy",
+    filtered: "false"
+  },
 
-//   {
-//     dish: "McDonalds",
-//     // images: "".
-//     description: "Western fast food, Burgers",
-//     filtered: "false"
-//   },
+  {
+    dish: "McDonalds",
+    // images: "".
+    description: "Western fast food, Burgers",
+    filtered: "false"
+  },
 
-//   {
-//     dish: "Sushi",
-//     // images: "".
-//     description: "Japanese food, Sushi",
-//     filtered: "false"
-//   },
+  {
+    dish: "Sushi",
+    // images: "".
+    description: "Japanese food, Sushi",
+    filtered: "false"
+  },
 
-//   {
-//     dish: "Fish and Chips",
-//     // images: "".
-//     description: "British food, Western food",
-//     filtered: "false"
-//   },
+  {
+    dish: "Fish and Chips",
+    // images: "".
+    description: "British food, Western food",
+    filtered: "false"
+  },
 
-//   {
-//     dish: "Steak",
-//     // images: "".
-//     description: "Best steak in Shanghai",
-//     filtered: "false"
-//   },
+  {
+    dish: "Steak",
+    // images: "".
+    description: "Best steak in Shanghai",
+    filtered: "false"
+  },
 
-//   {
-//     dish: "Papa John",
-//     // images: "".
-//     description: "Pizza, Western fast",
-//     filtered: "false"
-//   },
+  {
+    dish: "Papa John",
+    // images: "".
+    description: "Pizza, Western fast",
+    filtered: "false"
+  },
 
-//   {
-//     dish: "American BBQ",
-//     // images: "".
-//     description: "American style BBQ",
-//     filtered: "false"
-//   },
+  {
+    dish: "American BBQ",
+    // images: "".
+    description: "American style BBQ",
+    filtered: "false"
+  },
 
-//   {
-//     dish: "Korean Fried Chicken",
-//     // images: "".
-//     description: "Korean style, Fried Chicken",
-//     filtered: "false"
-//   },
-// ]
+  {
+    dish: "Korean Fried Chicken",
+    // images: "".
+    description: "Korean style, Fried Chicken",
+    filtered: "false"
+  },
+]
+
+let rand = DEFAULT[Math.floor(Math.random()*DEFAULT.length)];
+console.log(rand);
 
 const dishes = [1, 2, 3]
 
+// const LIKES = [];
+// const HATES = [];
+
 const AppWrapper = styled.div`
-  background:black;
+  background:white;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -87,7 +94,7 @@ const AppWrapper = styled.div`
 const LandingWrapper = styled.div`
   display:flex;
   flex-direction:column;
-  background:black;
+  background:lightgrey;
   justify-content: center;
   align-content: center
   height:100vh;
@@ -95,7 +102,7 @@ const LandingWrapper = styled.div`
 
 const Title = styled.h1`
   text-align: center;
-  color: red;
+  color: black;
 `
 
 const StartButton = styled.button`
@@ -111,7 +118,7 @@ const StartButton = styled.button`
 
 const AppDescription = styled.h2`
     text-align: center;
-    color: red;
+    color: black;
 `
 const FunctionPage = styled.div`
   display:flex;
@@ -133,27 +140,29 @@ const HateLibrary = styled.button`
   height: 10vh;
 `
 
-
 const ChoicesBox = styled.div`
     display:flex;
     flex-direction:column
     width:100vw;
     height:25vh;
-    background:black;
+    background:lightgrey;
     justify-content:center;
     align-item:center;
 `
 const Choices = styled.div`
     display: flex;
     flex-direction: row;
+    border-style:solid;
 `
 
 const ChoicesDescription = styled.div`
      display: flex;
      flex-direction: column;
      margin-left: auto;
-     background:blue;
+     background:lightgrey;
      padding:5px;
+     border-style:solid
+     width:500px;
 `
 
 const ViewMoreButton = styled.button`
@@ -198,6 +207,7 @@ function App() {
 
 function LandingPage() {
   const [click, setClick] = useState(0);
+  // const [chosen,setChosen] = useState([]);
 
   if (click === 0) {
     return (
@@ -216,32 +226,81 @@ function LandingPage() {
       <div>
         <Navi />
         <RunMainFunction />
-        <ShowMore>Show more choices</ShowMore>
+        <ShowMore onClick={RunMainFunction}>Show more choices</ShowMore>
       </div>
     );
   }
 }
 
+
 // This is the componets to run three times
 
 function RunMainFunction() {
+  let initialChosen = [];
+  initialChosen[0] = getNextRandomDish(initialChosen);
+  initialChosen[1] = getNextRandomDish(initialChosen);
+  initialChosen[2] = getNextRandomDish(initialChosen);
+
+  const [chosen,setChosen] = useState(initialChosen);
+
   return (
-    dishes.map(mainFunction)
+    <>
+      <MainFunction 
+        chosen={chosen} 
+        setChosen={setChosen}
+        dish={DEFAULT[initialChosen[0]]}
+      />
+      <MainFunction 
+        chosen={chosen} 
+        setChosen={setChosen}
+        dish={DEFAULT[initialChosen[1]]}
+
+      />
+      <MainFunction 
+        chosen={chosen} 
+        setChosen={setChosen}
+        dish={DEFAULT[initialChosen[2]]}
+    // chosen={chosen} setChosen={setChosen} 不会
+      />
+    </>
   );
+}
+
+//This is the function for getting three random number
+function getNextRandomDish(chosen) {
+
+  if (chosen.length === DEFAULT.length) {
+    return -1;
+  }
+
+  let first = -1;
+  let exist = false;
+  do {
+    
+    first = Math.floor(Math.random()*DEFAULT.length);
+
+    exist = chosen.indexOf(first) >= 0; // 这个不会
+  
+  } while (exist);
+
+  return first;
 }
 
 
 // This is the function showing one choice
 
-function mainFunction() {
+function MainFunction(props) {
+
+  console.log(props);
+
   return (
     <ChoicesBox>
       <Choices>
         <Title>Images</Title>
 
         <ChoicesDescription>
-          <Title> Description </Title>
-          <Title> Types </Title>
+          <Title> {props.dish.dish} </Title>
+          <Title> {props.dish.description} </Title>
         </ChoicesDescription>
 
         <ViewMoreButton> View more </ViewMoreButton>
@@ -252,8 +311,8 @@ function mainFunction() {
         <NoButton> ❌</NoButton>
       </ButtonBoxes>
 
-
     </ChoicesBox>
+    
   );
 }
 
